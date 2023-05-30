1becad3c5157fcd046aca0f70b8c593a5b19617b
bravadine <ygsmr.public@protonmail.com>
2023-05-31 04:41:53 +0700

    Major rewrite

--- NeedlemanWunsch.cpp
+++ NeedlemanWunsch.cpp
@@ -4,6 +4,33 @@
 #include <format>
 
 class NeedlemanWunsch {
+public:
+    std::string alignedSubject;
+    std::string alignedQuery;
+    int score;
+
+    NeedlemanWunsch(
+            const std::string_view subject,
+            const std::string_view query,
+            const int matchScore,
+            const int mismatchScore,
+            const int gapScore)
+            : subject(subject), query(query), matchScore(matchScore), mismatchScore(mismatchScore), gapScore(gapScore) {
+        initializeScoreMatrix();
+        calculateScoreMatrix();
+        traceBestAlignment();
+    }
+
+    ~NeedlemanWunsch() = default;
+
+    auto getScoreMatrix() const {
+        return scoreMatrix;
+    }
+
+    void viewScoreMatrix();
+
+    void viewBestAlignment();
+
 private:
     typedef struct MatrixCell {
         int score;
@@ -15,42 +42,37 @@ private:
 
     const std::string_view subject;
     const std::string_view query;
-    std::string alignedSubject;
-    std::string alignedQuery;
 
     const int matchScore;
     const int mismatchScore;
     const int gapScore;
-    int totalScore = 0;
 
-    MatrixCell calculateCell(const size_t x, const size_t y) {
+    void calculateCell(const size_t x, const size_t y) {
         auto u = scoreMatrix[x - 1][y].score + gapScore;
         auto l = scoreMatrix[x][y - 1].score + gapScore;
         auto d = scoreMatrix[x - 1][y - 1].score + (subject[x - 1] == query[y - 1] ? matchScore : mismatchScore);
 
-        auto score = std::max({u, l, d});
-        auto direction = score == u ? MatrixCell::UP : score == l ? MatrixCell::LEFT : MatrixCell::DIAGONAL;
-        return {score, direction};
+        auto cellScore = std::max({u, l, d});
+        auto cellTrace = cellScore == u ? MatrixCell::UP : cellScore == l ? MatrixCell::LEFT : MatrixCell::DIAGONAL;
+        scoreMatrix[x][y] = {cellScore, cellTrace};
     }
 
     void initializeScoreMatrix() {
         scoreMatrix.resize(subject.length() + 1, std::vector<MatrixCell>(query.length() + 1));
 
-        for (int col = 0; col < subject.length() + 1; ++col) {
-            scoreMatrix[col][0].score = gapScore * col;
-            scoreMatrix[col][0].trace = MatrixCell::UP;
+        for (int col = 0; col < scoreMatrix.size(); ++col) {
+            scoreMatrix[col][0] = {gapScore * col, MatrixCell::UP};
         }
 
-        for (int row = 0; row < query.length() + 1; ++row) {
-            scoreMatrix[0][row].score = gapScore * row;
-            scoreMatrix[0][row].trace = MatrixCell::LEFT;
+        for (int row = 0; row < scoreMatrix.size(); ++row) {
+            scoreMatrix[0][row] = {gapScore * row, MatrixCell::LEFT};
         }
     }
 
     void calculateScoreMatrix() {
         for (size_t x = 1; x <= subject.length(); ++x) {
             for (size_t y = 1; y <= query.length(); ++y) {
-                scoreMatrix[x][y] = calculateCell(x, y);
+                calculateCell(x, y);
             }
         }
     }
@@ -58,94 +80,61 @@ private:
     void traceBestAlignment() {
         size_t x = subject.length();
         size_t y = query.length();
-        alignedSubject = subject[x] + alignedSubject;
-        alignedQuery = query[y] + alignedQuery;
-        totalScore = scoreMatrix[x][y].score;
+
+        alignedSubject.reserve(x + y);
+        alignedQuery.reserve(x + y);
+
+        alignedSubject += subject[x];
+        alignedQuery += query[x];
+        score = scoreMatrix[x][y].score;
+
         while (x || y) {
             switch (scoreMatrix[x][y].trace) {
                 case MatrixCell::UP:
-                    alignedSubject = subject[--x] + alignedSubject;
-                    alignedQuery = '-' + alignedQuery;
+                    alignedSubject += subject[--x];
+                    alignedQuery += '-';
                     break;
                 case MatrixCell::LEFT:
-                    alignedSubject = '-' + alignedSubject;
-                    alignedQuery = query[--y] + alignedQuery;
+                    alignedSubject += '-';
+                    alignedQuery += query[--y];
                     break;
                 case MatrixCell::DIAGONAL:
-                    alignedSubject = subject[--x] + alignedSubject;
-                    alignedQuery = query[--y] + alignedQuery;
+                    alignedSubject += subject[--x];
+                    alignedQuery += query[--y];
                     break;
             }
-            totalScore += scoreMatrix[x][y].score;
+            score += scoreMatrix[x][y].score;
         }
-    }
 
-public:
-    NeedlemanWunsch(
-            const std::string_view subject,
-            const std::string_view query,
-            const int matchScore = 2,
-            const int mismatchScore = -1,
-            const int gapScore = -2)
-            : subject(subject), query(query), matchScore(matchScore), mismatchScore(mismatchScore), gapScore(gapScore) {
-        initializeScoreMatrix();
-        calculateScoreMatrix();
-        traceBestAlignment();
-    }
-
-    void printScoreMatrix() {
-        std::cout << "          ";
-        for (const auto &n: query) {
-            std::cout << std::format("  {}  ", n);
-        }
-        std::cout << "\n";
-        int n = -1;
-        for (const auto &column: scoreMatrix) {
-            std::cout << std::format("  {}  ", n == -1 ? ' ' : subject[n]);
-            for (const auto &row: column) {
-                std::cout << std::format(" {:>3} ", row.score);
-            }
-            std::cout << "\n";
-            ++n;
-        }
-    }
-
-    void printBestAlignment() {
-        std::cout << alignedSubject << "\n";
-        for (size_t c = 0; c < alignedSubject.length() - 1; ++c) {
-            std::cout << (alignedSubject[c] == alignedQuery[c] ? '|' : ' ');
-        }
-        std::cout << "\n" << alignedQuery << std::endl;
-    }
-
-    [[nodiscard]] std::string getSubject() const {
-        return std::string(subject);
-    }
-
-    [[nodiscard]] std::string getQuery() const {
-        return std::string(query);
-    }
-
-    [[nodiscard]] int getMatchScore() const {
-        return matchScore;
-    }
-
-    [[nodiscard]] int getMismatchScore() const {
-        return mismatchScore;
-    }
-
-    [[nodiscard]] int getGapScore() const {
-        return gapScore;
-    }
-
-    [[nodiscard]] int getTotalScore() const {
-        return totalScore;
-    }
-
-    [[nodiscard]] std::vector<std::vector<MatrixCell>> getScoreMatrix() const {
-        return scoreMatrix;
+        std::reverse(alignedSubject.begin(), alignedSubject.end());
+        std::reverse(alignedQuery.begin(), alignedQuery.end());
     }
 };
+
+void NeedlemanWunsch::viewScoreMatrix() {
+    std::cout << "          ";
+    for (const auto &n: query) {
+        std::cout << std::format("  {}  ", n);
+    }
+    std::cout << "\n";
+
+    int n = -1;
+    for (const auto &column: scoreMatrix) {
+        std::cout << std::format("  {}  ", (n++) == -1 ? ' ' : subject[n]);
+        for (const auto &row: column) {
+            std::cout << std::format(" {:>3} ", row.score);
+        }
+        std::cout << "\n";
+    }
+}
+
+void NeedlemanWunsch::viewBestAlignment() {
+    std::cout << alignedSubject << "\n";
+    for (size_t c = 0; c < alignedSubject.length() - 1; ++c) {
+        std::cout << (alignedSubject[c] == alignedQuery[c] ? '|' : ' ');
+    }
+    std::cout << "\n" << alignedQuery << std::endl;
+}
 
 int main() {
     const std::string strandA = "CACGTGATCAA";
@@ -156,19 +145,19 @@ int main() {
 
     NeedlemanWunsch NWInstance(strandA, strandB, matchScore, mismatchScore, gapScore);
 
-    std::cout << "STRAND #1: " << NWInstance.getSubject() << "\n";
-    std::cout << "STRAND #2: " << NWInstance.getQuery() << "\n\n";
+    std::cout << "STRAND #1: " << strandA << "\n";
+    std::cout << "STRAND #2: " << strandB << "\n\n";
 
     std::cout << "SCORING SCHEME:\n"
-                 "- MATCH     = " << NWInstance.getMatchScore() << "\n" <<
-                 "- MISMATCH  = " << NWInstance.getMismatchScore() << "\n" <<
-                 "- INDEL/GAP = " << NWInstance.getGapScore() << "\n\n";
+                 "- MATCH     = " << matchScore << "\n" <<
+                 "- MISMATCH  = " << mismatchScore << "\n" <<
+                 "- INDEL/GAP = " << gapScore << "\n\n";
 
     std::cout << "MATRIX:\n";
-    NWInstance.printScoreMatrix();
+    NWInstance.viewScoreMatrix();
     std::cout << "\nALIGNMENT:\n";
-    NWInstance.printBestAlignment();
-    std::cout << "\nSCORE: " << NWInstance.getTotalScore() << std::endl;
+    NWInstance.viewBestAlignment();
+    std::cout << "\nSCORE: " << NWInstance.score << std::endl;
 
     return 0;
 }
